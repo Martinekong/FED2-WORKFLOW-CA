a654b9b911b579203f4cd9ea6fe6c3f29aa203fd
Martinekong <martinekongsrud@outlook.com>
2025-05-05 16:06:52 +0200

    configured eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import vitest from 'eslint-plugin-vitest';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  {
+    files: ['tailwind.config.js', 'eslint.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    plugins: { vitest },
+    languageOptions: {
+      globals: globals.vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
+]);
